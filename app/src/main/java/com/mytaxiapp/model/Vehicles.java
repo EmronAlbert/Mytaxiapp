90a11ccb5570aab5da6b45211e37c3bc7b58a00a
tosinonikute <tosinmath007@gmail.com>
2017-08-13 16:09:00 +0100

    added POJO classes for Vehicles and Placemark

---
+++ app/src/main/java/com/mytaxiapp/model/Vehicles.java
@@ -0,0 +1,21 @@
+package com.mytaxiapp.model;
+
+/**
+ * @author Tosin Onikute.
+ */
+
+import java.util.List;
+
+public class Vehicles {
+
+    private List<Placemark> placemarks = null;
+
+    public List<Placemark> getPlacemarks() {
+        return placemarks;
+    }
+
+    public void setPlacemarks(List<Placemark> placemarks) {
+        this.placemarks = placemarks;
+    }
+
+}
